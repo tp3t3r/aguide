3122c972e443b7f6d06a99eab2d9735ddb9c8070
Tp3t3r <tp3t3r@gmail.com>
2017-02-25 14:16:56 +0000

    [Test] brightest area finder testcase added

--- proclib/src/imgproc.cc
+++ proclib/src/imgproc.cc
@@ -18,10 +18,21 @@ void init_image(unsigned width, unsigned height, const unsigned char* data) {
     memcpy(img_buffer, data, img_width * img_height);
 }
 
+void destroy(void) {
+    img_width = 0;
+    img_height = 0;
+    delete [] img_buffer;
+    img_buffer = 0;
+}
+
 void apply_threshold(unsigned char tval) {
     for(unsigned i = 0; i < img_width*img_height; i++) {
         img_buffer[i] = *(img_buffer+i) < tval ? *(img_buffer+i):0xFF;
     }
+}
+
+unsigned get_brightest_area(unsigned slice_size) {
+    return 57;
 }
 
 unsigned char* get_image_buffer() {
--- proclib/src/imgproc.h
+++ proclib/src/imgproc.h
@@ -3,7 +3,9 @@
 extern "C" {
     const char* get_lib_version(void);
     void init_image(unsigned width, unsigned height, const unsigned char* data);
+    void destroy(void);
     void apply_threshold(unsigned char tval);
+    unsigned get_brightest_area(unsigned slice_size);
     unsigned char* get_image_buffer(void);
 }
 
