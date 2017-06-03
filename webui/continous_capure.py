12287e8d54da163e22aca4db912cbc5efe6e4796
tp3t3r <tp3t3r@gmail.com>
2017-06-03 22:05:09 +0000

    capture: long exposures enabled

--- webui/continous_capure.py
+++ webui/continous_capure.py
@@ -1,23 +1,37 @@
 #!/usr/bin/env python
 
-import time
+import time,sys
 import picamera
 
 def filenames():
     frame = 0
     while True:
+        frame = frame + 1
+        filename = 'image%02d.png' % (frame % 2)
         time.sleep(0.5)
-        yield 'image00.png'
+        yield filename
 
-with picamera.PiCamera(framerate=2) as camera:
-    time.sleep(2)
-    camera.resolution=(640,480)
-    camera.vflip=True
-    camera.hflip=True   
-    camera.framerate=2
-    camera.color_effects = (128,128)
-    camera.ISO=200
-    camera.meter_mode = 'average'
 
-    camera.capture_sequence(filenames(), use_video_port=True, format='png')
+try:
+    with picamera.PiCamera() as camera:
+        camera.resolution=(640,480)
+        camera.vflip = True
+        camera.hflip = True
+        #camera.led = False
+        camera.framerate = 1
+        camera.exposure_mode ='off'
+        camera.color_effects = (128,128)
+        camera.shutter_speed = 400000
+        camera.ISO=800
+        camera.meter_mode = 'average'
+        print "getting ready..."
+        time.sleep(5)
+        print "go!"
+        camera.capture_sequence(filenames(), use_video_port=True, format='png')
+except KeyboardInterrupt:
+    print "exiting..."
+    sys.exit()
+except:
+    print "terrible thing happened"
+    sys.exit()
 
