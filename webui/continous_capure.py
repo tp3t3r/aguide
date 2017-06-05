026593b59891afcf2c703a410c5837ec0ad23e4f
tp3t3r <tp3t3r@gmail.com>
2017-06-05 14:17:10 +0200

    [UI] basic capture

--- webui/continous_capure.py
+++ webui/continous_capure.py
@@ -1,37 +1,42 @@
 #!/usr/bin/env python
 
-import time,sys
+import time,sys,os
 import picamera
-
-def filenames():
-    frame = 0
-    while True:
-        frame = frame + 1
-        filename = 'image%02d.png' % (frame % 2)
-        time.sleep(0.5)
-        yield filename
-
 
 try:
     with picamera.PiCamera() as camera:
+
         camera.resolution=(640,480)
+        #camera.zoom = (0.25,0.25,0.125,0.125)
         camera.vflip = True
         camera.hflip = True
         #camera.led = False
-        camera.framerate = 1
-        camera.exposure_mode ='off'
+        camera.framerate = 2
+        camera.awb_mode = 'sunlight'
+        
+        '''
+        camera.exposure_mode = 'off'
         camera.color_effects = (128,128)
         camera.shutter_speed = 400000
         camera.ISO=800
         camera.meter_mode = 'average'
+        '''
         print "getting ready..."
-        time.sleep(5)
+        time.sleep(3)
         print "go!"
-        camera.capture_sequence(filenames(), use_video_port=True, format='png')
+       
+
+        fname='evf.png'
+        fname_tmp='.evf.png' 
+        while True:
+            print time.time()
+            camera.capture('.evf.png', use_video_port=True, format='png')
+            os.rename('.evf.png', 'evf.png')
 except KeyboardInterrupt:
     print "exiting..."
     sys.exit()
-except:
+except Exception as e:
     print "terrible thing happened"
+    print e
     sys.exit()
 
