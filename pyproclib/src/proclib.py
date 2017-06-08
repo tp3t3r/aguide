8b40c16fa8b3bcd459e78659dd971ab8b40c8e26
tp3t3r <tp3t3r@gmail.com>
2017-06-08 23:30:11 +0200

    [test] python proclib wrapper

--- pyproclib/src/proclib.py
+++ pyproclib/src/proclib.py
@@ -1,49 +1,53 @@
 import ctypes
 
+#globals
+imgbuflen = 320 * 240
+bufptr = ctypes.POINTER(ctypes.c_ubyte * imgbuflen)
+
 class proclib():
     def __init__(self, so = "/tmp/libimgproc.so"):
         self.lib = ctypes.CDLL(so)
 
-        self.get_lib_version = self.lib.get_lib_version
-        self.get_lib_version.argtypes = None
-        self.get_lib_version.restype = ctypes.c_char_p
+        self.initImage = self.lib.init_image
+        self.initImage.argtypes = [ ctypes.c_uint, ctypes.c_uint, bufptr ]
+        self.initImage.restype = None
 
-        #void init_image(unsigned width, unsigned height, const unsigned char* data);
-        self.init_image = self.lib.init_image
-        self.init_image.argtypes = [ ctypes.c_uint, ctypes.c_uint, ctypes.c_char_p ]
-        self.init_image.restype = None
-
-        #void apply_threshold(unsigned char tval);
-        self.apply_threshold = self.lib.apply_threshold
-        self.apply_threshold.argtypes = [ ctypes.c_ubyte ]
-        self.apply_threshold.restype = None
+        self.applyThreshold = self.lib.apply_threshold
+        self.applyThreshold.argtypes = [ ctypes.c_ubyte ]
+        self.applyThreshold.restype = None
 
         #void get_spot_coordinates(int *x, int *y);
-        self.get_spot_coordinates = self.lib.get_spot_coordinates
-        self.get_spot_coordinates.argtypes = [ ctypes.c_void_p, ctypes.c_void_p ]
-        self.get_spot_coordinates.restype = None
+        self.getSpotCoordinates = self.lib.get_spot_coordinates
+        self.getSpotCoordinates.argtypes = [ ctypes.c_void_p, ctypes.c_void_p ]
+        self.getSpotCoordinates.restype = None
 
-        #const unsigned char* get_image_buffer(void);
-        self.get_image_buffer = self.lib.get_image_buffer
-        self.get_image_buffer.argtypes = None
-        self.apply_threshold.restype = ctypes.c_char_p
+        self.getBuffer = self.lib.get_image_buffer
+        self.getBuffer.argtypes = None
+        #self.getBuffer.restype = bufptr
+        self.getBuffer.restype =  ctypes.POINTER(ctypes.c_ubyte)
+
+from PIL import Image
+import sys
+testimage = Image.open(sys.argv[1])
+tlum = testimage.convert('L')
+data = list(tlum.getdata())
+
+plib = proclib()
+plib.initImage(320,240,(ctypes.c_ubyte * len(data))(*data) )        
+buffer1 = plib.getBuffer()
+plib.applyThreshold(220)
+print buffer2[100:130]
+
+x = ctypes.c_int() 
+y = ctypes.c_int()
+plib.getSpotCoordinates(ctypes.byref(x), ctypes.byref(y))
+plib.getSpotCoordinates(ctypes.byref(x), ctypes.byref(y))
+
+plib.getSpotCoordinates(ctypes.byref(x), ctypes.byref(y))
+
+plib.getSpotCoordinates(ctypes.byref(x), ctypes.byref(y))
+
+plib.getSpotCoordinates(ctypes.byref(x), ctypes.byref(y))
 
 
-    def getVer(self):
-        return self.get_lib_version()
-
-    def getSpotCoordinates(self):
-        x = ctypes.c_void_p()
-        y = ctypes.c_void_p()
-        self.get_spot_coordinates(x, y)
-        return x.contents, y.contents
-
-    def applyThreshold(self, imagedata, tval = 200):
-        raw_data_ptr = (ctypes.c_ubyte * len(imagedata))(*imagedata)
-        width=640
-        height=480
-        dataptr = self.apply_threshold(raw_data_ptr, width, height, tval)
-        dataptr = ctypes.cast(dataptr, ctypes.POINTER(ctypes.c_ubyte))
-        return [dataptr[i] for i in range(width*height)]
-
-
+print "spot: ", x.value, ":", y.value
