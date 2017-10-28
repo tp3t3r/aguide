5aaad284c87c19f4c082546df564b33e64fac9e9
tp3t3r <tp3t3r@gmail.com>
2017-10-28 11:01:05 +0200

    [proclib] new method

--- proclib/src/ImageProcessor.cc
+++ proclib/src/ImageProcessor.cc
@@ -30,6 +30,7 @@ void ImageProcessor::addFrame(const char* data) {
 void ImageProcessor::setThreshold(int th) {
     _threshold = th;
 }
+
 void ImageProcessor::getBrightestSlice(int *x_area, int *y_area, int *brightness) {
     memset(_slice_weights, 0, _slice_count*sizeof(_slice_count));
     int iy, ix;
@@ -68,39 +69,47 @@ void ImageProcessor::getBrightestSlice(int *x_area, int *y_area, int *brightness
 }
 
 void ImageProcessor::getSpotCoordinates(int * x, int * y, bool isLocked) {
+    int ix, iy;
+    int x_offset, y_offset;
+    int sumx=0;
+    int sumy=0;
+    int pixelCount = 0; //above threshold
+
     if (!isLocked) {
-        *x = -1;
-        *y = -1;
-    }
-    static int x_area, y_area, brightness;
-    if (!isLocked) {
+        int x_area, y_area, brightness;
+        //get the brightest part first
         getBrightestSlice(&x_area, &y_area, &brightness);
         if ( x_area < 0 || y_area < 0 || brightness < 1) {
             //printf("not bright spot");
             return;
         }
+        pixelCount = brightness;
+        x_offset = x_area * _slice_size;
+        y_offset = y_area * _slice_size;
+    } else {
+        //not locked, check the neighbors of (given) X:Y
+        //TODO: check if they're on the edge of the frame
+        x_offset = *x - _slice_size / 2;
+        y_offset = *y - _slice_size / 2;
     }
-    int iy,ix;
-    int x_offset = x_area * _slice_size;
-    int y_offset = y_area * _slice_size;
-    int sumx = 0;
-    int sumy = 0;
-
     //iterate over the selected area, calculate coordinate averages
     for(iy=y_offset; iy < y_offset + _slice_size; iy++) {
         for(ix=x_offset; ix < x_offset + _slice_size; ix++) {
             int pixel = *(_data+ix+iy*_width);
             //printf("%03d ", pixel);
             if(pixel > _threshold) {
+                if (isLocked) {
+                    pixelCount++;
+                }
                 sumx += ix;
                 sumy += iy;
             }
         }
         //printf("\n");
     }
-    //printf("spot: %d:%d [%d]\n", sumx/brightness, sumy/brightness, brightness);
-    *x = sumx/brightness;
-    *y = sumy/brightness;
+    //printf("spot: %d:%d [%d]\n", sumx/pixelCount, sumy/pixelCount, pixelCount);
+    *x = sumx/pixelCount;
+    *y = sumy/pixelCount;
 }
 
 const char* ImageProcessor::getBuffer(void) {
