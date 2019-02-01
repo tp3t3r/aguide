8c98911b26a705e2bb07034032a0af3fe6880dab
tp3t3r <tp3t3r@gmail.com>
2019-02-01 23:55:11 +0100

    [stepper] moved to easy driver from native H bridge

--- guider2/stepperdriver.py
+++ guider2/stepperdriver.py
@@ -4,32 +4,23 @@ import RPi.GPIO as GPIO ## Import GPIO library
 #type
 # 17hd34008-22b
 
-#stepper motor wiring:
-#out
-#L298 header VCC/NC A/RED B/GREEN C/BLUE D/BLACK
-
-#in
-#INA/PUPRLE INB/BLUE INC/GREEN IND/YELLOW
-
-#common
-#G on raspi is connected to driver's common
-
 # on raspi:
+# GN - ground
+# DR - direction
+# ST = step
 # 2  4  6  8 10 12 14 16 18 20 22 24 26 18 30 32 34 36 38 40
-#|  |+5|GN|  |  |P |  |  |  |  |  |  |  |  |  |Y |  |G |B |P |
+#|+5|  |GN|DR|ST|  |  |  |  |  |  |  |  |  |  |  |  |  |  |  |
 #=============================================================
 #|  |  |  |  |  |  |  |  |  |  |  |  |  |  |  |  |  |  |  |  |
 
 class StepperDriver():
     def __init__(self):
         GPIO.setmode(GPIO.BCM) ## Use board pin numbering
-        self.used_pins = [12, 16, 20, 21]
-        self.sequence = [[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]]
-        self.delay = 0.05
-
+        self.used_pins = [14, 15]
         for pin in self.used_pins:
             GPIO.setup(pin, GPIO.OUT)
             GPIO.output(pin, GPIO.LOW)
+        self.delay = 0.1
 
     def __enter__(self):
         pass
@@ -42,32 +33,22 @@ class StepperDriver():
     def doStep(self, count):
         if count > 0:
             while count:
+                self.setPins(0, 1)
+                time.sleep(self.delay)
+                self.setPins(0, 0)
                 count -= 1
-                for step in self.sequence:
-                    self.setPins(step[0], step[1], step[2], step[3])
-                    time.sleep(self.delay)
         else:
             while count:
+                self.setPins(1, 1)
+                time.sleep(self.delay)
                 count += 1
-                for step in self.sequence[::-1]:
-                    self.setPins(step[0], step[1], step[2], step[3])
-                    time.sleep(self.delay)
 
-    def setPins(self, p1, p2, p3, p4):
-        if p1:
-            GPIO.output(12, GPIO.HIGH)
+    def setPins(self, p_dir, p_step):
+        if p_dir:
+            GPIO.output(14, GPIO.HIGH)
         else:
-            GPIO.output(12, GPIO.LOW)
-        if p2:
-            GPIO.output(16, GPIO.HIGH)
+            GPIO.output(14, GPIO.LOW)
+        if p_step:
+            GPIO.output(15, GPIO.HIGH)
         else:
-            GPIO.output(16, GPIO.LOW)
-
-        if p3:
-            GPIO.output(20, GPIO.HIGH)
-        else:
-            GPIO.output(20, GPIO.LOW)
-        if p4:
-            GPIO.output(21, GPIO.HIGH)
-        else:
-            GPIO.output(21, GPIO.LOW)
+            GPIO.output(15, GPIO.LOW)
