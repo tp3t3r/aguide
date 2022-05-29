eac964327d4dd61eb502d411014714eac83ccdf1
tp3t3r <tp3t3r@gmail.com>
2022-05-29 13:31:50 +0200

    start from scratch

--- env/.vimrc
+++
@@ -1,7 +0,0 @@
-set tabstop=4 
-set shiftwidth=4 
-set softtabstop=4 
-set expandtab      
-color elflord
-syntax on
-set number
--- playground/stepperDriver.py
+++
@@ -1,8 +0,0 @@
-class stepperDriver:
-    def __init__(self,threshold):
-        self.threshold = threshold
-        pass
-    def act(self, movement):
-        if movement >= self.threshold:
-            #move
-            pass
